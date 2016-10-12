695b595af86eb6b3daa307f162848794bf35cb4e
sajjadsad <rd27gc@gmail.com>
2016-10-13 01:10:56 +0300

    Add files via upload

---
+++ data/config.lua
@@ -0,0 +1,63 @@
+do local _ = {
+  about_text = "! Masters Bot 2.1v 🔰\n\nThe advanced administration bot based on Tg-Cli. 🌐\n\nIt was built on a platform TeleSeed after it has been modified.🔧🌐\n\nhttps://github.com/MastersDev\n\nProgrammer🔰\n@iDev1\n\nSpecial thanks to😋❤️\nTeleSeed Team\nMico \nMouamle\nOscar\n\nOur channels 😍👍🏼\n@MastersDev 🌚⚠️\n@OSCARBOTv2 🌚🔌\n@MouamleAPI 🌚🔩\n@Malvoo 🌚🔧\n \nMy YouTube Channel\nhttps://www.youtube.com/channel/UCKsJSbVGNGyVYvV5B2LrUkA",
+  enabled_plugins = {
+    "admin",
+    "ingroup",
+    "stats",
+    "anti_spam",
+    "all",
+    "msg_checks",
+    "badword",
+    "robot",
+    "getfile",
+    "newgroup",
+    "leave_ban",
+    "lock_bot",
+    "lock_user",
+    "me",
+    "moody",
+    "save",
+    "uu",
+    "info",
+    "html",
+    "lock_inl",
+    "banhammer",
+    "vip",
+    "supergroup",
+    "lock_fwd",
+    "sudo",
+    "ps",
+    "mee",
+    "lock_tag",
+    "lock_emoji",
+    "lock_cmd",
+    "broadcast",
+    "ons",
+    "kickme"
+  },
+  help_text = "",
+  help_text_realm = "",
+  help_text_super = "",
+  moderation = {
+    data = "data/moderation.json"
+  },
+  sudo_users = {
+    67369633,
+    225159095,
+    260424796,
+    209194833,
+    230220539,
+    176001217,
+    210114356,
+    112476673,
+    79956882,
+    79956882,
+    94840171,
+    58492415,
+    32784046,
+    290489741,
+    112476673
+  }
+}
+return _
+end
